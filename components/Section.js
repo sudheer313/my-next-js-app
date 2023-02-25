18b19c353666d5ce3bc7e402ed54dc4f73140dfc
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-02-25 14:04:52 +1100

    update and componnets folder with Header and section and also add styles to section

---
+++ components/Section.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "@/styles/Home.module.css";
+const Section = () => {
+  return (
+    <section className={styles.section}>
+      <h1>Hi there this is my first next.js app</h1>
+    </section>
+  );
+};
+
+export default Section;
